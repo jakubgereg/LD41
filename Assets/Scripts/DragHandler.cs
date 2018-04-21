ef8ad6057972b43bea78547823f3d9277fe8b48f
Jakub Gereg <j.gereg@gmail.com>
2018-04-21 18:59:08 +1200

    Testing drag and drop stuff
    works kinda ok

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public static GameObject itemDragged;
+    private Vector3 startPosition;
+    private Transform startParent;
+
+    public GameObject toSpawn;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        //GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        transform.position = Input.mousePosition;
+    }
+
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemDragged = null;
+        //GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+        }
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.position);
+        startParent = null;
+        transform.position = eventData.position;
+
+        var pos = Camera.main.ScreenToWorldPoint(eventData.position);
+
+        Vector3 newpost = new Vector3(pos.x, pos.y, 1);
+
+        toSpawn.transform.position = newpost;
+
+        toSpawn.SetActive(true);
+
+        Instantiate(toSpawn);
+        Destroy(gameObject);
+    }
+}
